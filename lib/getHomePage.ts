36b9fc260a18365b1bfba0a55d40260e99301655
Harsh Mishra <151397568+Harsh-Mishra0@users.noreply.github.com>
2026-06-15 20:12:53 +0530

    Delete lib directory

--- lib/getHomePage.ts
+++
@@ -1,9 +0,0 @@
-import { fetchAPI } from "./api";
-
-export async function getHomePage() {
-  const data = await fetchAPI<{ data: unknown[] }>(
-    "/pages?filters[slug][$eq]=home&populate[content][populate]=*"
-  );
-
-  return data.data[0];
-}
